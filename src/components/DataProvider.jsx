b03902ae366eb2d1159d8ba5f1407b606246b613
prasadgit07143 <prasad07143.vrsec@gmail.com>
2024-07-17 10:20:21 +0530

    Initial Commit

---
+++ src/components/DataProvider.jsx
@@ -0,0 +1,46 @@
+const videoData = [
+  {
+    src: "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
+    title: "Never Gonna Give You Up",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
+    title: "Just the Way You Are",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
+    title: "Big Buck Bunny",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8",
+    title: "Kiss Me",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
+    title: "Carrier in Peak",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
+    title: "No Way Home",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+  {
+    src: "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
+    title: "Cup in the Woods",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
+    tags: ["music", "video", "funny"],
+  },
+];
+
+export default videoData;
---
+++ src/components/Shorts.jsx
@@ -0,0 +1,235 @@
+import React, { useState, useRef, useEffect, useCallback, memo } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPause,
+  faPlay,
+  faVolumeHigh,
+  faVolumeXmark,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faShare,
+  faInfo,
+} from "@fortawesome/free-solid-svg-icons";
+import videoData from "./DataProvider";
+import "video.js/dist/video-js.css";
+
+const Short = memo(({ video, isVisible, index }) => {
+  const [playState, setPlayState] = useState(isVisible);
+  const [muteState, setMuteState] = useState(true);
+  const [likeState, setLikeState] = useState(
+    () => localStorage.getItem(`like_video_${index}`) === "true"
+  );
+  const [progress, setProgress] = useState(0);
+  const videoRef = useRef(null);
+
+  const updatePlayState = useCallback(() => {
+    setPlayState((prevState) => {
+      const newState = !prevState;
+      videoRef.current?.[newState ? "play" : "pause"]();
+      return newState;
+    });
+  }, []);
+
+  const updateMuteState = useCallback(() => {
+    setMuteState((prevState) => {
+      const newState = !prevState;
+      if (videoRef.current) videoRef.current.muted = newState;
+      return newState;
+    });
+  }, []);
+
+  const handleLike = useCallback(() => {
+    setLikeState((prevState) => {
+      const newState = !prevState;
+      localStorage.setItem(`like_video_${index}`, newState.toString());
+      return newState;
+    });
+  }, [index]);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    if (isVisible) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+      setPlayState(true);
+    } else {
+      videoRef.current.pause();
+      setPlayState(false);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const updateProgress = () => {
+      setProgress((video.currentTime / video.duration) * 100);
+    };
+
+    video.addEventListener("timeupdate", updateProgress);
+    return () => video.removeEventListener("timeupdate", updateProgress);
+  }, []);
+
+  return (
+    <div onDoubleClick={handleLike} className="short | flex jcc fdc">
+      <video
+        className="video-js"
+        preload="auto"
+        data-setup="{}"
+        onClick={updatePlayState}
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+        loop
+      >
+        <source type="application/x-mpegurl" src={video.src} />
+      </video>
+      <div className="video-progress-wrapper | flex aife">
+        <div
+          className="video-progress-bar"
+          style={{
+            width: `${progress}%`,
+          }}
+        ></div>
+      </div>
+
+      <div className="video-details | w-100">
+        <span className="video-title | fz-14 fw-400">{video.title}</span>
+        <br />
+        {video.tags.map((tag) => (
+          <span key={tag} className="video-tag">{`#${tag} `}</span>
+        ))}
+      </div>
+      <div className="video-controls | flex aic w-100 fz-18">
+        <button onClick={updatePlayState} className="video-play-pause-btn">
+          <FontAwesomeIcon icon={playState ? faPause : faPlay} />
+        </button>
+        <button onClick={updateMuteState} className="video-mute-unmute-btn">
+          <FontAwesomeIcon icon={muteState ? faVolumeXmark : faVolumeHigh} />
+        </button>
+      </div>
+      <div className="video-info-wrapper | flex aic jcfe fdc">
+        <div
+          onClick={handleLike}
+          className={`video-info likes-wrapper | center fz-24 ${
+            likeState ? "likes-wrapper-active" : ""
+          }`}
+        >
+          <span>
+            <FontAwesomeIcon icon={faHeart} />
+          </span>
+        </div>
+        <div className="video-info share-wrapper | center">
+          <span>
+            <FontAwesomeIcon icon={faShare} />
+          </span>
+        </div>
+        <div className="video-info info-wrapper | center">
+          <span>
+            <FontAwesomeIcon icon={faInfo} />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+const Shorts = () => {
+  const [visibleIndex, setVisibleIndex] = useState(() => {
+    const savedIndex = localStorage.getItem("lastSeenVideoIndex");
+    return savedIndex !== null ? parseInt(savedIndex, 10) : 0;
+  });
+  const sectionRef = useRef(null);
+
+  const handleScroll = useCallback(() => {
+    if (!sectionRef.current) return;
+
+    const updateVisibleIndex = () => {
+      const shorts = sectionRef.current.querySelectorAll(".short");
+      const sectionRect = sectionRef.current.getBoundingClientRect();
+      const sectionCenter = sectionRect.top + sectionRect.height / 2;
+
+      shorts.forEach((short, index) => {
+        const rect = short.getBoundingClientRect();
+        const shortCenter = rect.top + rect.height / 2;
+        if (Math.abs(sectionCenter - shortCenter) < rect.height / 2) {
+          setVisibleIndex(index);
+        }
+      });
+    };
+
+    requestAnimationFrame(updateVisibleIndex);
+  }, []);
+
+  const scrollToIndex = useCallback((index) => {
+    sectionRef.current
+      ?.querySelectorAll(".short")
+      [index]?.scrollIntoView({ behavior: "smooth", block: "center" });
+  }, []);
+
+  const scrollToPrevious = useCallback(() => {
+    setVisibleIndex((prevIndex) => {
+      const newIndex = Math.max(0, prevIndex - 1);
+      scrollToIndex(newIndex);
+      return newIndex;
+    });
+  }, [scrollToIndex]);
+
+  const scrollToNext = useCallback(() => {
+    setVisibleIndex((prevIndex) => {
+      const newIndex = Math.min(videoData.length - 1, prevIndex + 1);
+      scrollToIndex(newIndex);
+      return newIndex;
+    });
+  }, [scrollToIndex]);
+
+  useEffect(() => {
+    localStorage.setItem("lastSeenVideoIndex", visibleIndex.toString());
+  }, [visibleIndex]);
+
+  useEffect(() => {
+    scrollToIndex(visibleIndex);
+  }, [visibleIndex, scrollToIndex]);
+
+  useEffect(() => {
+    const section = sectionRef.current;
+    if (!section) return;
+
+    section.addEventListener("scroll", handleScroll);
+    scrollToIndex(visibleIndex);
+    handleScroll();
+
+    return () => section.removeEventListener("scroll", handleScroll);
+  }, [handleScroll, scrollToIndex, visibleIndex]);
+
+  return (
+    <section className="shorts-section | h-100 center" ref={sectionRef}>
+      <div className="short-wrapper | ff-roboto flex aic fdc">
+        {videoData.map((video, videoIndex) => (
+          <Short
+            key={videoIndex}
+            video={video}
+            isVisible={videoIndex === visibleIndex}
+            index={videoIndex}
+          />
+        ))}
+        <div className="video-nav-controls | flex fdc jcsb fz-24">
+          <div onClick={scrollToPrevious} className="nav-btn-wrapper | center">
+            <span className="nav-btn prev">
+              <FontAwesomeIcon icon={faAngleUp} />
+            </span>
+          </div>
+          <div onClick={scrollToNext} className="nav-btn-wrapper | center">
+            <span className="nav-btn next">
+              <FontAwesomeIcon icon={faAngleDown} />
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Shorts;
