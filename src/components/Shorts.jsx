53be410480f7cf5e71ff9ba0871e8c5267e64bc5
prasadgit07143 <prasad07143.vrsec@gmail.com>
2024-07-21 00:13:04 +0530

    - New Commit By Prasad Meesala

--- src/components/Shorts.jsx
+++ src/components/Shorts.jsx
@@ -1,4 +1,11 @@
-import React, { useState, useRef, useEffect, useCallback, memo } from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+  memo,
+  useMemo,
+} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPause,
@@ -9,187 +16,265 @@ import {
   faAngleDown,
   faHeart,
   faShare,
-  faInfo,
+  faEllipsisVertical,
+  faExpand,
+  faCompress,
 } from "@fortawesome/free-solid-svg-icons";
 import videoData from "./DataProvider";
 import shortVideo from "../assets/videos/big_buck_bunny_720p_1mb.mp4";
 
-const Short = memo(({ video, isVisible, index }) => {
-  const [playState, setPlayState] = useState(isVisible);
-  const [muteState, setMuteState] = useState(true);
-  const [likeState, setLikeState] = useState(
-    () => localStorage.getItem(`like_video_${index}`) === "true"
-  );
-  const [progress, setProgress] = useState(0);
-  const videoRef = useRef(null);
+const Short = memo(
+  ({ video, isVisible, index, resetInfo, isFullScreen, setFullScreen }) => {
+    const [playState, setPlayState] = useState(isVisible);
+    const [muteState, setMuteState] = useState(true);
+    const [likeState, setLikeState] = useState(
+      () => localStorage.getItem(`like_video_${index}`) === "true"
+    );
+    const [progress, setProgress] = useState(0);
+    const [controlsVisible, setControlsVisible] = useState(false);
+    const [infoState, setInfoState] = useState(false);
+    const videoRef = useRef(null);
 
-  const updatePlayState = useCallback(() => {
-    setPlayState((prevState) => {
-      const newState = !prevState;
-      videoRef.current?.[newState ? "play" : "pause"]();
-      return newState;
-    });
-  }, []);
+    const toggleFullScreen = useCallback(
+      () => setFullScreen((prev) => !prev),
+      [setFullScreen]
+    );
+    const updatePlayState = useCallback(
+      () =>
+        setPlayState((prev) => {
+          const newState = !prev;
+          videoRef.current?.[newState ? "play" : "pause"]();
+          return newState;
+        }),
+      []
+    );
+    const updateMuteState = useCallback(
+      () =>
+        setMuteState((prev) => {
+          const newState = !prev;
+          if (videoRef.current) videoRef.current.muted = newState;
+          return newState;
+        }),
+      []
+    );
+    const handleLike = useCallback(
+      () =>
+        setLikeState((prev) => {
+          const newState = !prev;
+          localStorage.setItem(`like_video_${index}`, newState.toString());
+          return newState;
+        }),
+      [index]
+    );
 
-  const updateMuteState = useCallback(() => {
-    setMuteState((prevState) => {
-      const newState = !prevState;
-      if (videoRef.current) videoRef.current.muted = newState;
-      return newState;
-    });
-  }, []);
+    useEffect(() => {
+      if (!videoRef.current) return;
+      if (isVisible) {
+        videoRef.current.currentTime = 0;
+        const playPromise = videoRef.current.play();
+        if (playPromise !== undefined) {
+          playPromise.then(() => setPlayState(true)).catch(() => {});
+        }
+      } else {
+        videoRef.current.pause();
+        setPlayState(false);
+      }
+    }, [isVisible]);
 
-  const handleLike = useCallback(() => {
-    setLikeState((prevState) => {
-      const newState = !prevState;
-      localStorage.setItem(`like_video_${index}`, newState.toString());
-      return newState;
-    });
-  }, [index]);
+    useEffect(() => {
+      const video = videoRef.current;
+      if (!video) return;
+      const updateProgress = () =>
+        setProgress((video.currentTime / video.duration) * 100);
+      video.addEventListener("timeupdate", updateProgress);
+      return () => video.removeEventListener("timeupdate", updateProgress);
+    }, []);
 
-  useEffect(() => {
-    if (!videoRef.current) return;
-    if (isVisible) {
-      videoRef.current.currentTime = 0;
-      videoRef.current.play();
-      setPlayState(true);
-    } else {
-      videoRef.current.pause();
-      setPlayState(false);
-    }
-  }, [isVisible]);
+    const handleMouseLeave = useCallback(() => setControlsVisible(false), []);
+    const handleMouseEnter = useCallback(() => setControlsVisible(true), []);
 
-  useEffect(() => {
-    const video = videoRef.current;
-    if (!video) return;
+    useEffect(() => {
+      let timeoutId;
+      if (controlsVisible && playState) {
+        timeoutId = setTimeout(() => setControlsVisible(false), 1200);
+      }
+      return () => clearTimeout(timeoutId);
+    }, [controlsVisible, playState]);
 
-    const updateProgress = () => {
-      setProgress((video.currentTime / video.duration) * 100);
-    };
+    useEffect(() => {
+      if (resetInfo) setInfoState(false);
+    }, [resetInfo]);
 
-    video.addEventListener("timeupdate", updateProgress);
-    return () => video.removeEventListener("timeupdate", updateProgress);
-  }, []);
+    const videoClassName = useMemo(
+      () => (isFullScreen ? "short-no-border-radius" : ""),
+      [isFullScreen]
+    );
+    const progressWrapperClassName = useMemo(
+      () =>
+        `video-progress-wrapper | flex aife w-100${
+          isFullScreen ? " short-no-border-radius padding-zero" : ""
+        }`,
+      [isFullScreen]
+    );
 
-  return (
-    <div onDoubleClick={handleLike} className="short | flex jcc fdc">
-      {/* <video
-        className="video-js"
-        preload="auto"
-        data-setup="{}"
-        onClick={updatePlayState}
-        ref={videoRef}
-        autoPlay
-        muted
-        playsInline
-        loop
+    return (
+      <div
+        className={`short | flex jcc fdc w-100${
+          isFullScreen ? " short-full-width" : ""
+        }`}
       >
-        <source type="application/x-mpegurl" src={video.src} />
-      </video> */}
-      <video
-        preload="auto"
-        onClick={updatePlayState}
-        ref={videoRef}
-        autoPlay
-        muted
-        playsInline
-        loop
-        src={shortVideo}
-      ></video>
-      <div className="video-progress-wrapper | flex aife">
         <div
-          className="video-progress-bar"
-          style={{
-            width: `${progress}%`,
-          }}
-        ></div>
-      </div>
-
-      <div className="video-details | w-100">
-        <span className="video-title | fz-14 fw-400">{video.title}</span>
-        <br />
-        {video.tags.map((tag) => (
-          <span key={tag} className="video-tag">{`#${tag} `}</span>
-        ))}
-      </div>
-      <div className="video-controls | flex aic w-100 fz-18">
-        <button onClick={updatePlayState} className="video-play-pause-btn">
-          <FontAwesomeIcon icon={playState ? faPause : faPlay} />
-        </button>
-        <button onClick={updateMuteState} className="video-mute-unmute-btn">
-          <FontAwesomeIcon icon={muteState ? faVolumeXmark : faVolumeHigh} />
-        </button>
-      </div>
-      <div className="video-info-wrapper | flex aic jcfe fdc">
+          onClick={toggleFullScreen}
+          className="full-screen-toggle-box | center fz-24"
+        >
+          <FontAwesomeIcon icon={isFullScreen ? faCompress : faExpand} />
+        </div>
+        <video
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onDoubleClick={handleLike}
+          className={videoClassName}
+          preload="auto"
+          onClick={updatePlayState}
+          ref={videoRef}
+          autoPlay
+          muted
+          playsInline
+          loop
+          src={shortVideo}
+        />
+        <div className={progressWrapperClassName}>
+          <div
+            className="video-progress-bar"
+            style={{ width: `${progress}%` }}
+          />
+        </div>
         <div
-          onClick={handleLike}
-          className={`video-info likes-wrapper | center fz-24 ${
-            likeState ? "likes-wrapper-active" : ""
+          className={`video-details | w-100${
+            isFullScreen ? " short-no-border-radius" : ""
           }`}
         >
-          <span>
-            <FontAwesomeIcon icon={faHeart} />
-          </span>
+          <span className="video-title | fz-14 fw-400">{video.title}</span>
+          <br />
+          {video.tags.map((tag) => (
+            <span key={tag} className="video-tag">{`#${tag} `}</span>
+          ))}
         </div>
-        <div className="video-info share-wrapper | center">
-          <span>
-            <FontAwesomeIcon icon={faShare} />
-          </span>
+        <div
+          className="video-controls | flex aic w-100 fz-18"
+          style={{
+            opacity: !playState || controlsVisible ? 1 : 0,
+            transition: "opacity 0.15s linear",
+          }}
+        >
+          <button onClick={updatePlayState} className="video-play-pause-btn">
+            <FontAwesomeIcon icon={playState ? faPause : faPlay} />
+          </button>
+          <button onClick={updateMuteState} className="video-mute-unmute-btn">
+            <FontAwesomeIcon icon={muteState ? faVolumeXmark : faVolumeHigh} />
+          </button>
         </div>
-        <div className="video-info info-wrapper | center">
-          <span>
-            <FontAwesomeIcon icon={faInfo} />
-          </span>
+        <div className="video-info-wrapper | flex aic jcfe fdc">
+          <div
+            onClick={handleLike}
+            className={`video-info likes-wrapper | center fz-24 ${
+              likeState ? "likes-wrapper-active" : ""
+            }`}
+          >
+            <span>
+              <FontAwesomeIcon icon={faHeart} />
+            </span>
+          </div>
+          <div className="video-info share-wrapper | center">
+            <span>
+              <FontAwesomeIcon icon={faShare} />
+            </span>
+          </div>
+          <div
+            onClick={() => setInfoState((prev) => !prev)}
+            className="video-info info-wrapper | center"
+          >
+            <span>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </span>
+          </div>
+        </div>
+        <div
+          onClick={() => setInfoState(false)}
+          className={`video-desc-wrapper | fdc${infoState ? " flex" : ""}`}
+        >
+          <h1 className="video-title | fz-20">{video.title}</h1>
+          <div className="hor-line" />
+          <p className="video-desc">{video.desc}</p>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 const Shorts = () => {
   const [visibleIndex, setVisibleIndex] = useState(() => {
     const savedIndex = localStorage.getItem("lastSeenVideoIndex");
     return savedIndex !== null ? parseInt(savedIndex, 10) : 0;
   });
+  const [newVideoScrolled, setNewVideoScrolled] = useState(false);
+  const [hasPrev, setHasPrev] = useState(() => visibleIndex > 0);
+  const [hasNext, setHasNext] = useState(
+    () => visibleIndex < videoData.length - 1
+  );
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const sectionRef = useRef(null);
 
   const handleScroll = useCallback(() => {
     if (!sectionRef.current) return;
-
     const updateVisibleIndex = () => {
       const shorts = sectionRef.current.querySelectorAll(".short");
-      const sectionRect = sectionRef.current.getBoundingClientRect();
-      const sectionCenter = sectionRect.top + sectionRect.height / 2;
-
+      const scrollPosition = sectionRef.current.scrollTop;
+      const windowHeight = window.innerHeight;
       shorts.forEach((short, index) => {
-        const rect = short.getBoundingClientRect();
-        const shortCenter = rect.top + rect.height / 2;
-        if (Math.abs(sectionCenter - shortCenter) < rect.height / 2) {
+        const shortTop = short.offsetTop;
+        const shortHeight = short.offsetHeight;
+        const shortCenter = shortTop + shortHeight / 2;
+        const viewportCenter = scrollPosition + windowHeight / 2;
+        if (
+          Math.abs(viewportCenter - shortCenter) < shortHeight / 2 &&
+          index !== visibleIndex
+        ) {
+          setNewVideoScrolled(true);
           setVisibleIndex(index);
         }
       });
     };
-
     requestAnimationFrame(updateVisibleIndex);
-  }, []);
+  }, [visibleIndex]);
+
+  useEffect(() => {
+    if (newVideoScrolled) setNewVideoScrolled(false);
+  }, [newVideoScrolled]);
 
   const scrollToIndex = useCallback((index) => {
-    sectionRef.current
-      ?.querySelectorAll(".short")
-      [index]?.scrollIntoView({ behavior: "smooth", block: "center" });
+    if (!sectionRef.current) return;
+    const shorts = sectionRef.current.querySelectorAll(".short");
+    const targetShort = shorts[index];
+    if (targetShort) {
+      const targetTop =
+        targetShort.offsetTop -
+        (window.innerHeight - targetShort.offsetHeight) / 2;
+      sectionRef.current.scrollTo({ top: targetTop, behavior: "smooth" });
+    }
   }, []);
 
   const scrollToPrevious = useCallback(() => {
-    setVisibleIndex((prevIndex) => {
-      const newIndex = Math.max(0, prevIndex - 1);
+    setVisibleIndex((prev) => {
+      const newIndex = Math.max(0, prev - 1);
       scrollToIndex(newIndex);
       return newIndex;
     });
   }, [scrollToIndex]);
 
   const scrollToNext = useCallback(() => {
-    setVisibleIndex((prevIndex) => {
-      const newIndex = Math.min(videoData.length - 1, prevIndex + 1);
+    setVisibleIndex((prev) => {
+      const newIndex = Math.min(videoData.length - 1, prev + 1);
       scrollToIndex(newIndex);
       return newIndex;
     });
@@ -197,6 +282,8 @@ const Shorts = () => {
 
   useEffect(() => {
     localStorage.setItem("lastSeenVideoIndex", visibleIndex.toString());
+    setHasPrev(visibleIndex !== 0);
+    setHasNext(visibleIndex !== videoData.length - 1);
   }, [visibleIndex]);
 
   useEffect(() => {
@@ -206,32 +293,41 @@ const Shorts = () => {
   useEffect(() => {
     const section = sectionRef.current;
     if (!section) return;
-
     section.addEventListener("scroll", handleScroll);
     scrollToIndex(visibleIndex);
     handleScroll();
-
     return () => section.removeEventListener("scroll", handleScroll);
   }, [handleScroll, scrollToIndex, visibleIndex]);
 
   return (
-    <section className="shorts-section | h-100 center" ref={sectionRef}>
-      <div className="short-wrapper | ff-roboto flex aic fdc">
+    <section className="shorts-section | h-100 w-100" ref={sectionRef}>
+      <div className="shorts-wrapper | ff-roboto flex aic fdc">
         {videoData.map((video, videoIndex) => (
           <Short
             key={videoIndex}
             video={video}
             isVisible={videoIndex === visibleIndex}
             index={videoIndex}
+            resetInfo={newVideoScrolled}
+            isFullScreen={isFullScreen}
+            setFullScreen={setIsFullScreen}
           />
         ))}
         <div className="video-nav-controls | flex fdc jcsb fz-24">
-          <div onClick={scrollToPrevious} className="nav-btn-wrapper | center">
+          <div
+            onClick={scrollToPrevious}
+            className="nav-btn-wrapper | center"
+            style={{ visibility: !hasPrev && "hidden" }}
+          >
             <span className="nav-btn prev">
               <FontAwesomeIcon icon={faAngleUp} />
             </span>
           </div>
-          <div onClick={scrollToNext} className="nav-btn-wrapper | center">
+          <div
+            onClick={scrollToNext}
+            className="nav-btn-wrapper | center"
+            style={{ visibility: !hasNext && "hidden" }}
+          >
             <span className="nav-btn next">
               <FontAwesomeIcon icon={faAngleDown} />
             </span>
